41eac42eb1f50420f0793dd5f34870c8a3a188f5
xrw002 <xrw001@gmail.com>
2024-08-10 08:52:22 +0800

    Update check_vless.sh

--- vless/check_vless.sh
+++ vless/check_vless.sh
@@ -36,16 +36,8 @@ generate_uuid() {
 
     # 预定义的UUID列表
     predefined_uuids=(
-        "fb210b24-46dd-4b4c-92ce-097385945dad"
-        "53cfcb07-8c25-4c25-baaa-95b4b50871a2"
-        "445ae56f-727d-495e-9c88-cbe942d144a6"
-        "078eb39d-2094-4272-b221-782ba0520dd6"
-        "5826e9cc-c5b7-49ca-8b37-a0ea68f382cc"
-        "e79fda4a-9519-4ef3-8973-130801b3d0ae"
-        "c0422b3b-00aa-4dbe-8573-6fb15d49e557"
-        "907e3ac9-02de-47fe-b40c-c2bd912c3adf"
-        "c53ca34c-8d9c-4a7e-8b44-5da52e4b5eaa"
-        "73fc0a2d-2458-435b-92aa-b4e8e3e40944"
+        "3ad3cc8a-3f02-47e0-ac10-35b64ddcfd82"
+        
     )
     uuid=${predefined_uuids[$RANDOM % ${#predefined_uuids[@]}]}
     echo "$uuid"
